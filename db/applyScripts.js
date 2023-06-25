2a7983463b785666f0ba9d831ef8629f2ea1dbb8
Lucas Gimenes Koritar <lucas.koritar@gwcloud.company>
2023-06-25 15:33:34 -0300

    test localhost

--- db/applyScripts.js
+++ db/applyScripts.js
@@ -5,7 +5,7 @@ const path = require('path');
 async function applyScript() {
   const pool = new Pool({
     user: process.env.DB_USER,
-    host: process.env.DB_HOST,
+    host: 'localhost',
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
