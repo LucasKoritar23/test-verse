04cc1259b6515b321b9cd515214d14cb79034854
Lucas Gimenes Koritar <lucas.koritar@gwcloud.company>
2023-06-25 15:06:16 -0300

    fix in credentials

--- db/applyScripts.js
+++ db/applyScripts.js
@@ -3,15 +3,15 @@ const fs = require('fs');
 
 // Configurações de conexão com o banco de dados
 const dbConfig = {
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    user: "$DB_USER",
+    host: "$DB_HOST",
+    database: "$DB_DATABASE",
+    password: "$DB_PASSWORD",
+    port: "$DB_PORT",
 };
 
 // Ler o conteúdo do arquivo SQL
-const scriptContent = fs.readFileSync('/var/jenkins_home/workspace/pipeline-test-verse/db/suite.sql', 'utf8');
+const scriptContent = fs.readFileSync('./suite.sql', 'utf8');
 
 // Função para executar o script SQL
 async function executeScript() {
