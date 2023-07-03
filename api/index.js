baeba64630c23fa90fd836d0236e2f3761185ac1
Lucas Gimenes Koritar <lucas.koritar@gwcloud.company>
2023-07-03 11:22:49 -0300

    add prometheus metrics

--- api/index.js
+++ api/index.js
@@ -2,11 +2,20 @@ const express = require('express');
 const routesSuites = require('./suites/routes');
 const routesTestCase = require('./test-case/routes');
 const swagger = require('./swagger');
+const prometheusApiMetrics = require('prometheus-api-metrics');
 
 const app = express();
 const port = 3001; // Porta do microserviço suites
 
 app.use(express.json());
+
+// Adicione o middleware do prometheus-api-metrics
+app.use(prometheusApiMetrics());
+
+// Middleware para incrementar o contador a cada request
+app.use((req, res, next) => {
+  next();
+});
 
 app.use('/suites', routesSuites);
 app.use('/test-cases', routesTestCase);
